e3306ab0fc22cb0c668d9c14225c9269f8b54f91
Chris Jones <cmsj@tenshu.net>
2022-10-10 16:38:48 +0100

    Update for samba commandline options that no longer exist

--- src/samba.sh
+++ src/samba.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ionice -c 3 nmbd -FS --no-process-group </dev/null &
-ionice -c 3 smbd -FS --no-process-group </dev/null
+ionice -c 3 nmbd -F --debuglevel=1 --no-process-group </dev/null &
+ionice -c 3 smbd -F --debuglevel=1 --no-process-group </dev/null
 
