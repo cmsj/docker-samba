b21393272801deac0dbacd0f9eb26353e4173c92
Chris Jones <cmsj@tenshu.net>
2022-10-10 17:58:49 +0100

    This is awful, it should be in the ENV somehow

--- src/samba.sh
+++ src/samba.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+addgroup --gid 1001 smbarchive
+
+adduser --home /tmp/ --uid 1026 --gid 100 --disabled-login media
+adduser --home /tmp/ --uid 1027 --gid 1001 --disabled-login  smbarchive
+
 ionice -c 3 nmbd -F --debuglevel=1 --debug-stdout --no-process-group </dev/null &
 ionice -c 3 smbd -F --debuglevel=1 --debug-stdout --no-process-group </dev/null
 
