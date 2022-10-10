e4d3ac3f783e7f3ccef418e431134f61dd0fcc2a
Chris Jones <cmsj@tenshu.net>
2022-10-10 22:53:58 +0100

    Add avahi service

--- src/samba.sh
+++ src/samba.sh
@@ -5,6 +5,9 @@ addgroup --gid 1001 smbarchive
 adduser --home /tmp/ --uid 1026 --gid 100 --disabled-login media
 adduser --home /tmp/ --uid 1027 --gid 1001 --disabled-login  smbarchive
 
+cp /samba.service /etc/avahi/services
+
 ionice -c 3 nmbd -F --debuglevel=1 --debug-stdout --no-process-group </dev/null &
 ionice -c 3 smbd -F --debuglevel=1 --debug-stdout --no-process-group </dev/null
 
+rm /etc/avahi/services/samba.service
