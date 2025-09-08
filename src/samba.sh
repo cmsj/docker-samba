c1b636c6ea0dca40208e24297646b5b1e799732a
Chris Jones <cmsj@tenshu.net>
2025-09-08 17:22:20 +0100

    Add a user

--- src/samba.sh
+++ src/samba.sh
@@ -2,10 +2,12 @@
 
 addgroup --gid 1001 smbarchive
 addgroup --gid 1031 smbjasper
+addgroup --gid 1032 smbniklas
 
 adduser --uid 1026 --gid 100 --disabled-login --gecos "" media
 adduser --uid 1027 --gid 1001 --disabled-login  --gecos "" smbarchive
 adduser --uid 1029 --gid 1031 --disabled-login --gecos "" smbjasper
+adduser --uid 1030 --gid 1032 --disabled-login --gecos "" smbniklas
 
 cp /samba.service /etc/avahi/services
 
